4245a7c4238e22093ec52d526497623eec3add77
Johnny <tutktoto05@gmail.com>
2024-11-09 15:35:44 +0300

    cookie fix

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { globalCookiesConfig } from "vue3-cookies";
+
+globalCookiesConfig({
+  expireTimes: "30d",
+  path: "/",
+  domain: "",
+  secure: true,
+  sameSite: "None",
+});
 
 createApp(App).mount('#app')
